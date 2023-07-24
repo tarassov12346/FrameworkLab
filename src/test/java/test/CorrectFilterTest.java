da628486f4e26571ffce450e660f40ff259e941e
Andrei Tarassov <tamogenia@gmail.com>
2023-07-24 15:22:46 +0300

    Added streams to test

--- src/test/java/test/CorrectFilterTest.java
+++ src/test/java/test/CorrectFilterTest.java
@@ -1,45 +1,34 @@
 package test;
 
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.List;
+import java.util.stream.IntStream;
 
-public class CorrectFilterTest extends CommonConditions{
+public class CorrectFilterTest extends CommonConditions {
     @Test(description = "checks the selected items to be of given color")
     public void doSelectedItemsCorrespondToFilterConditionsOfColor() {
         setNumberOfPagesForColor();
-        if (numberOfPages>0){
-            for (int j = 0; j < numberOfPages; j++) {
-                List<WebElement> filteredElementsList = filterItemsByColorBoxTick(j);
-                int numberOfFilteredElements = filteredElementsList.size();
-                for (int i = 0; i < numberOfFilteredElements; i++) {
-                    filteredElementsList = filterItemsByColorBoxTick(j);
-                    filteredElementsList.get(i).click();
+        if (numberOfPages > 0) {
+            IntStream.range(0, numberOfPages).boxed().toList().forEach(pageIndex -> {
+                IntStream.range(0, filterItemsByColorBoxTick(pageIndex).size()).boxed().toList().forEach(elementIndex -> {
+                    filterItemsByColorBoxTick(pageIndex).get(elementIndex).click();
                     Assert.assertTrue(getItemColoursAsString().contains(String.format("background-color: rgb(%s);", CHECK_COLOUR)));
-                }
-            }
+                });
+            });
         }
-
     }
 
     @Test(description = "checks the selected items to be of given price rank")
     public void doSelectedItemsCorrespondToFilterConditionsOfPriceRank() {
         setNumberOfPagesForPriceRank(LOWER_PRICE, UPPER_PRICE);
-        if (numberOfPages>0){
-            for (int j = 0; j < numberOfPages; j++) {
-                List<WebElement> filteredElementsList = filterItemsByGivenPriceRank(LOWER_PRICE, UPPER_PRICE,j);
-                int numberOfFilteredElements = filteredElementsList.size();
-
-                for (int i = 0; i < numberOfFilteredElements; i++) {
-                    filteredElementsList = filterItemsByGivenPriceRank(LOWER_PRICE, UPPER_PRICE,j);
-                    filteredElementsList.get(i).click();
+        if (numberOfPages > 0) {
+            IntStream.range(0, numberOfPages).boxed().toList().forEach(pageIndex -> {
+                IntStream.range(0, filterItemsByGivenPriceRank(LOWER_PRICE, UPPER_PRICE, pageIndex).size()).boxed().toList().forEach(elementIndex -> {
+                    filterItemsByGivenPriceRank(LOWER_PRICE, UPPER_PRICE, pageIndex).get(elementIndex).click();
                     Assert.assertTrue((getItemPrice() >= LOWER_PRICE_DOUBLE_VALUE) && (getItemPrice() <= UPPER_PRICE_DOUBLE_VALUE));
-
-                }
-            }
+                });
+            });
         }
-
     }
 }
--- src/test/java/test/CorrectSearchTest.java
+++ src/test/java/test/CorrectSearchTest.java
@@ -9,21 +9,21 @@ import java.util.Locale;
 public class CorrectSearchTest extends CommonConditions {
     @Test(description = "compares the search request one and the selected items names")
     public void doSelectedItemsCorrespondToSearchRequestOne() {
-        getItemsBySearchRequest(SEARCH_REQUEST_ONE).stream().
+        getItemsBySearchRequest(SEARCH_REQUEST_ONE).
                 forEach(item -> Assert.assertTrue(item.getText().toLowerCase(Locale.ROOT).
                         contains(SearchRequestReader.getSearchData(SEARCH_REQUEST_ONE).toLowerCase(Locale.ROOT))));
     }
 
     @Test(description = "compares the search request two and the selected items names")
     public void doSelectedItemsCorrespondToSearchRequestTwo() {
-        getItemsBySearchRequest(SEARCH_REQUEST_TWO).stream().
+        getItemsBySearchRequest(SEARCH_REQUEST_TWO).
                 forEach(item -> Assert.assertTrue(item.getText().toLowerCase(Locale.ROOT).
                         contains(SearchRequestReader.getSearchData(SEARCH_REQUEST_TWO).toLowerCase(Locale.ROOT))));
     }
 
     @Test(description = "compares the search request three and the selected items names")
     public void doSelectedItemsCorrespondToSearchRequestThree() {
-        getItemsBySearchRequest(SEARCH_REQUEST_THREE).stream().
+        getItemsBySearchRequest(SEARCH_REQUEST_THREE).
                 forEach(item -> Assert.assertTrue(item.getText().toLowerCase(Locale.ROOT).
                         contains(SearchRequestReader.getSearchData(SEARCH_REQUEST_THREE).toLowerCase(Locale.ROOT))));
     }
