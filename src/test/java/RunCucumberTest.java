94c9169f0ff900a4e11374c5a1d508f4b42998c8
Andrei Tarassov <tamogenia@gmail.com>
2023-08-10 09:09:29 +0300

    Added integration of cucumber with reportportal

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,7 +1,7 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"pretty"},
+@CucumberOptions(plugin = {"pretty", "com.epam.reportportal.cucumber.StepReporter"},
         features = "src/test/resources/features/attributes")
 
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
